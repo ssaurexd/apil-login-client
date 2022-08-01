a92f457b73802122deda961013438dc8423efd82
Aure <ssaurexd@gmail.com>
2022-07-31 21:09:51 -0500

    arreglado errores heroku

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = process.env.NODE_ENV === 'production' ? process.env.API_HOST : 'http://localhost:4000/api'
+const baseURL = process.env.NODE_ENV === 'production' ? `${ process.env.API_HOST }/api` : 'http://localhost:4000/api'
 export const api = axios.create({
 	baseURL,
 	headers: {
