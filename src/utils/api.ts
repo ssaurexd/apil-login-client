3cb52feccfcd790cdf39374ac28d6be82ae60618
Aure <ssaurexd@gmail.com>
2022-08-01 11:24:58 -0500

    .

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,9 @@
 import axios from 'axios'
+/*  */
+import { apiHost } from '../config'
 
-const baseURL = process.env.NODE_ENV === 'production' ? `${ process.env.API_HOST }/api` : 'http://localhost:4000/api'
+
+const baseURL = process.env.NODE_ENV === 'production' ? `${ apiHost }api` : 'http://localhost:4000/api'
 export const api = axios.create({
 	baseURL,
 	headers: {
